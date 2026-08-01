ab461a1f8a1f46da7423fd69eeef36cf612df09d
PREMADAS <premadas083@gmail.com>
2026-08-01 14:02:37 +0600

    server-side

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const express = require('express')
-const app = express()
-const PORT = 5000
 
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+require('dotenv').config();
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const PORT = process.env.PORT;
+
+const uri = process.env.MONGODB_URI;
+
+
+
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+
+async function connectDB() {
+    try {
+        await client.connect();
+        await client.db('admin').command({ ping: 1 });
+        console.log('Pinged your deployment. You successfully connected to MongoDB!');
+    } catch (err) {
+        console.error(err);
+    }
+}
+connectDB();
 
 app.get('/', (req, res) => {
     res.send("Server is here")
@@ -11,3 +42,6 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+//s3jdtScOiOvW5c9P
+//car-rental-server

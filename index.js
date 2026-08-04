1aee891bcd80f00665bf566e597ae3907831503e
PREMADAS <premadas083@gmail.com>
2026-08-04 11:05:56 +0600

    server-side

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 require('dotenv').config();
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const PORT = process.env.PORT;
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
 
 const uri = process.env.MONGODB_URI;
-
-
-
-
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -22,26 +21,37 @@ const client = new MongoClient(uri, {
     },
 });
 
+app.get('/', (req, res) => {
+    res.send("Server is here");
+});
 
-async function connectDB() {
+async function run() {
     try {
         await client.connect();
         await client.db('admin').command({ ping: 1 });
         console.log('Pinged your deployment. You successfully connected to MongoDB!');
+
+        const carCollection = client.db("Car-Rental").collection("available-car");
+
+        app.get('/cars', async (req, res) => {
+            const result = await carCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.get('/cars/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await carCollection.findOne(query);
+            res.send(result);
+        });
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+
     } catch (err) {
         console.error(err);
     }
 }
-connectDB();
 
-app.get('/', (req, res) => {
-    res.send("Server is here")
-})
-
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
-
-//s3jdtScOiOvW5c9P
-//car-rental-server
+run();
